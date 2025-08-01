23732e77d2e31e59177eea4b82c62667a789f578
wea-f <100387337+wea-f@users.noreply.github.com>
2025-07-31 21:02:55 -0400

    Create autoclicker.js

---
+++ bookmarklets/autoclicker.js
@@ -0,0 +1,52 @@
+function autoclicker () {
+	var DELAYa = prompt("Directions: \n 1. Enter CPS \n 2. Click an element \n 3. Click the same spot to stop. \n \n Enter the CPS (clicks per second): "); 
+	var DELAY = 1000 / DELAYa;
+	
+	var autoClickerStyleElement = document.createElement("style");
+	autoClickerStyleElement.innerHTML = "*{cursor: crosshair !important;}";
+	document.body.appendChild(autoClickerStyleElement);
+
+	function addClicker(e) {
+		if (!e.isTrusted) {
+			return;
+		}
+		
+		// Remove crosshair cursor and event listener
+		document.body.removeChild(autoClickerStyleElement);
+		document.body.removeEventListener("click", addClicker);
+		e.preventDefault();
+		
+		e.target.classList.add("auto-clicker-target");
+		
+		// Add click listener to the target element for starting/stopping
+		e.target.addEventListener("click", function(clickEvent) {
+			if (!clickEvent.isTrusted) {
+				return;
+			}
+			
+			if (e.target.classList.contains("auto-clicker-active")) {
+				// Stop autoclicking
+				e.target.classList.remove("auto-clicker-active");
+			} else {
+				// Start autoclicking
+				e.target.classList.add("auto-clicker-active");
+				autoClick(e.target);
+			}
+			clickEvent.preventDefault();
+		});
+		
+		
+	}
+
+	function autoClick(element) {
+		if (element.classList.contains("auto-clicker-active")) {
+			element.click();
+			setTimeout(function() {
+				autoClick(element);
+			}, DELAY);
+		}
+	}
+	
+	document.body.addEventListener("click", addClicker, 0);
+	
+}
